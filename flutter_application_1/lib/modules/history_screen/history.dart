cc1d0210dad19e284b7d8f4d0bb7b12fbbfbd2ff
Youssef Ahmed <ya9951638@gmail.com>
2022-04-22 06:10:32 +0200

    history data

--- flutter_application_1/lib/modules/history_screen/history.dart
+++ flutter_application_1/lib/modules/history_screen/history.dart
@@ -3,20 +3,16 @@
 import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/history_model/histoy_model.dart';
-import 'package:flutter_application_1/models/user_model/Register_model.dart';
 import 'package:flutter_application_1/modules/cubit/homecubit_cubit.dart';
 import 'package:flutter_application_1/modules/cubit/homecubit_state.dart';
-import 'package:flutter_application_1/modules/history_screen/cubit/History_cubit.dart';
-import 'package:flutter_application_1/modules/history_screen/cubit/History_state.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class History extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<HomecubitCubit, HomecubitState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
+      listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
@@ -31,59 +27,73 @@ class History extends StatelessWidget {
           body: Padding(
             padding: const EdgeInsets.all(20.0),
             child: ConditionalBuilder(
-                condition: State is! HomecubitLoading,
-                // HomecubitCubit.get(context).model != null,
+              condition: State is! HomecubitBottomNav &&
+                  HomecubitCubit.get(context).model != null,
+              // HomecubitCubit.get(context).model != null,
 
-                builder: (context) => ListView.separated(
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (context, index) =>
-                        Historyview(HomecubitCubit.get(context).model),
-                    separatorBuilder: (context, index) => SizedBox(
-                          height: 15,
-                        ),
-                    itemCount:
-                        HomecubitCubit.get(context).model.data.payments.length),
-                fallback: (context) =>
-                    Center(child: CircularProgressIndicator())),
+              builder: (context) => ListView.separated(
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) =>
+                    Historyview(HomecubitCubit.get(context).model),
+                separatorBuilder: (context, index) => SizedBox(height: 20),
+                itemCount: 1,
+                // HomecubitCubit.get(context).model.data.payments.length,
+              ),
+              fallback: (context) => Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
           ),
         );
       },
     );
   }
-  //   ),
-  // );
 }
-// }
 
-Historyview(HistoryModel model) => model.data.payments.map((e) => Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        Text(
-          e.company_name,
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Row(
+Widget Historyview(HistoryModel model) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: model.data.payments.map(
+      (e) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Text(e.date, maxLines: 1),
+            Text(
+              e.company_name ?? 'null',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                width: 7.0,
-                height: 7.0,
-                decoration:
-                    BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
-              ),
+            SizedBox(
+              height: 5,
             ),
-            Text(e.total),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    // model.data.payments.map((e) => e.date).toString(),
+                    e.date ?? 'null',
+                    maxLines: 1,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                    width: 7.0,
+                    height: 7.0,
+                    decoration: BoxDecoration(
+                        color: Colors.blue, shape: BoxShape.circle),
+                  ),
+                ),
+                Text(
+                    // model.data.payments.map((e) => e.total).toString(),
+                    e.total ?? 'null'),
+              ],
+            ),
+            Divider(color: Colors.grey, thickness: 2)
           ],
-        ),
-      ],
-    ));
+        );
+      },
+    ).toList(),
+  );
+}
